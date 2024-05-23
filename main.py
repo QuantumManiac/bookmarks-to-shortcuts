857c414783593389d3e7969884cedbcf8f5d8a52
Chamath Wijesekera <chamath2.wijesekera@gmail.com>
2024-05-23 13:41:47 -0400

    Begin work on netscape bookmark file parsing

--- main.py
+++ main.py
@@ -3,10 +3,21 @@ import os
 from bookmarks_to_shortcuts import load_bookmarks, mirror_bookmarks
 
 def main():
-    parser = argparse.ArgumentParser(description="Mirrors Chrome bookmarks to your local filesystem, replicating the directory structure and converting bookmarks into internet shortcuts.")
+    parser = argparse.ArgumentParser(description="Mirrors browser bookmarks to your local filesystem, replicating the directory structure and converting bookmarks into internet shortcuts.")
     parser.add_argument("input", help="The input file to read bookmarks from.")
+    parser.add_argument("-f", "--format", choices=["chromium", "netscape"], help="The format of the input file. Can either be the Chromium Bookmarks file or a Netscape Bookmark File. If not specified, the format will be inferred from the file extension.")
     parser.add_argument("-o", "--output", help="The output folder to write the bookmarks to. If not specified, the output folder will be the current working directory.")
     args = parser.parse_args()
+
+    # If format is not specified, infer it from the file extension
+    if not args.format:
+        _, ext = os.path.splitext(args.input)
+        if ext == ".html":
+            args.format = "netscape"
+            print("file extension is html. Assuming this is a Netscape Bookmark File")
+        else:
+            args.format = "chromium"
+            print("file extension is not html. Assuming this is a Chromium Bookmarks file")
 
     bookmarks = load_bookmarks(args.input)
 
